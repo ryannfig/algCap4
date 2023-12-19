95d06d612fc23dc3b7a1752eb7af99049e438aa2
ryannfig <113732663+ryannfig@users.noreply.github.com>
2023-12-19 09:43:26 -0300

    Update QuadradoCuboQ4.java

--- QuadradoCuboQ4.java
+++ QuadradoCuboQ4.java
@@ -5,11 +5,9 @@ public class QuadradoCubo {
     public static void main(String[] args) {
       
  
-        // Solicita um valor usando JOptionPane
         String valorString = JOptionPane.showInputDialog("Digite um valor:");
         int valor = Integer.parseInt(valorString);
 
-        // Calcula o quadrado se o valor for par, ou o cubo se for ímpar
         int resultado;
         if (valor % 2 == 0) {
             resultado = valor * valor; // Quadrado
